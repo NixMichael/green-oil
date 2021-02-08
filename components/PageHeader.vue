86605c312215c6da4acae01c32ea96a83302eef9
Michael Nix <mpnix@protonmail.com>
2021-02-08 17:33:36 +0000

    Resized header image

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -98,7 +98,7 @@ export default {
 
   #header-image {
     border-top: 1px solid white;
-    min-height: 200px;
+    max-height: 200px;
     background-image: url('../assets/IMAGES/Logo.png');
     background-position-x: left;
     background-position-y: top;
